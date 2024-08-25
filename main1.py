d064555481870c9fbc59666d29d5bbbb1f26ce3b
Luis Fernando Garcia <107887072+LuisitoYsusAmigos@users.noreply.github.com>
2024-08-25 10:39:27 -0400

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,13 +1,31 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import moduloPreguntas
+from CreaacionEmbedingPDF import creacionEmbeding
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def root():
-    return "root" 
+    return "root"
+
+
+@app.route('/crear', methods=['GET'])
+def crear_pregunta():
+    respuesta =creacionEmbeding()
+    
+    
+
+    # Respuesta que incluye la pregunta recibida
+    
+
+    # Devuelve la respuesta como JSON
+    return jsonify({"respuesta": respuesta
+                    #,"pregunta": pregunta
+                    })
+
+
 
 @app.route('/pregunta', methods=['POST'])
 def recibir_pregunta():
