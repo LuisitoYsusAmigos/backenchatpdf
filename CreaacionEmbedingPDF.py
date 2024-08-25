6823c77a89b32ae11caca77359789fde68c64f60
Luis Fernando Garcia <107887072+LuisitoYsusAmigos@users.noreply.github.com>
2024-08-25 10:38:29 -0400

    Add files via upload

--- CreaacionEmbedingPDF.py
+++ CreaacionEmbedingPDF.py
@@ -3,44 +3,47 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from sentence_transformers import SentenceTransformer
 from langchain_community.vectorstores.faiss import FAISS
-pdf_file_obj = open('Antecedentes.pdf', 'rb')
-pdf_reader = PdfReader(pdf_file_obj)
-text = ""
-for page in pdf_reader.pages:
-    text += page.extract_text()
-#print(text[8000:10000])
 
-#crear chunks
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=70,
-    length_function=len
-    )
-chunks = text_splitter.split_text(text)
-print(len(chunks))
-# descargar modelo de embedding
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
-print("llego bien")
+def creacionEmbeding():
+    pdf_file_obj = open('Antecedentes.pdf', 'rb')
+    pdf_reader = PdfReader(pdf_file_obj)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    #print(text[8000:10000])
 
-#pruebas de embddings
-model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+    #crear chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=70,
+        length_function=len
+        )
+    chunks = text_splitter.split_text(text)
+    print(len(chunks))
+    # descargar modelo de embedding
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+    print("llego bien")
 
-# fin de la prueba
-knowledge_base = FAISS.from_texts(chunks, embeddings)
+    #pruebas de embddings
+    model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
 
-#print(docs)
+    # fin de la prueba
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+    #print(docs)
 
 
-import os
-import pickle
+    import os
+    import pickle
 
-# Obtener la ruta del directorio donde se encuentra el archivo .py
-script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Obtener la ruta del directorio donde se encuentra el archivo .py
+    script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Ruta del archivo .pkl
-pkl_path = os.path.join(script_dir, 'knowledge_base.pkl')
+    # Ruta del archivo .pkl
+    pkl_path = os.path.join(script_dir, 'knowledge_base.pkl')
 
-# Guardar el objeto knowledge_base en un archivo
-with open(pkl_path, 'wb') as f:
-    pickle.dump(knowledge_base, f)
+    # Guardar el objeto knowledge_base en un archivo
+    with open(pkl_path, 'wb') as f:
+        pickle.dump(knowledge_base, f)
+    return 'knowledge_base.pkl'
 
